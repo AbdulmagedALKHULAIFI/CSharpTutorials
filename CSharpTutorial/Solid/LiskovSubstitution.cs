8bfe8d934f3af794476f329d8a3ed1a2b39bfd4f
AbdulmagedALKHULAIFI <43535031+AbdulmagedALKHULAIFI@users.noreply.github.com>
2021-11-16 16:31:34 +0100

    Added Soild principale examples folder

---
+++ CSharpTutorial/Solid/LiskovSubstitution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTutorial.Solid
+{
+    class LiskovSubstitution
+    {
+        //Definition: you should be able to use any derived class instead of a parent class
+        //and have it behave in the same manner without modification
+    }
+}
